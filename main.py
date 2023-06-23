9e62f0319b0795d191731b826f1c444295ad1a0a
Cody Gardner <35705693+cagardner@users.noreply.github.com>
2023-06-22 18:21:50 -0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import config
-
-def main_menu():
-    flag = False
-    while not flag:
-        config_menu = config.ConfigurationMenu()
-        config_menu.display_menu()
-if __name__ == "__main__":
-    main_menu()
